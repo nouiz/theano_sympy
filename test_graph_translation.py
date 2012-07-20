0484e63d9d043e6bce94bb132c0f38a5ff82423d
Matthew Rocklin <mrocklin@gmail.com>
2012-07-20 10:17:41 -0500

    Added tests

--- test_graph_translation.py
+++ test_graph_translation.py
@@ -9,23 +9,34 @@ xs = sympy.Symbol('x')
 ys = sympy.Symbol('y')
 zs = sympy.Add(xs, ys)
 
-print "transformed graph"
-print theano_to_sympy(xt)
-print theano_to_sympy(zt)
-print sympy.simplify(theano_to_sympy(tt.cos(xt)**2 +
-                                     tt.sin(xt)**2))
+def test_theano_to_sympy():
 
-var_map = {xs.name: ('float32', (False, False)),
-           ys.name: ('float32', (False, False)),
-       }
-print sympy_to_theano(xs, var_map)
+    assert theano_to_sympy(xt) == xs
+    assert theano_to_sympy(zt) == zs
+    assert theano_to_sympy(tt.cos(xt)**2 +    tt.sin(xt)**2) == \
+                        sympy.cos(xs)**2 + sympy.sin(xs)**2
 
-print sympy_to_theano(xs + ys, var_map)
-print theano.printing.pprint(
-    sympy_to_theano(sympy.cos(xs)**2 + sympy.sin(xs)**2, var_map))
+    var_map = {xs.name: ('float32', (False, False)),
+               ys.name: ('float32', (False, False)),
+           }
+def test_shape_and_dtype_map():
+    assert shape_and_dtype_map(xt) == {'x': ('float64', ())}
+    assert shape_and_dtype_map(zt) == {'x': ('float64', ()),
+                                       'y': ('float64', ())}
+    assert shape_and_dtype_map(tt.matrix('name')) == \
+            {'name': ('float64', (False, False))}
 
-assert shape_and_dtype_map(xt) == {'x': ('float64', ())}
-zt = tt.cos(xt)**2 + tt.sin(xt)**2
-zs = theano_to_sympy(zt)
-m = shape_and_dtype_map(zt)
-print sympy_to_theano(sympy.simplify(zs), m)
+def test_sympy_to_theano():
+    m = shape_and_dtype_map(zt)
+
+    def theano_eq(a,b):
+        return a.type == b.type and a.name == b.name
+
+    assert theano_eq(sympy_to_theano(xs, m), xt)
+    assert theano_eq(sympy_to_theano(zs, m), zt)
+
+def test_sin_cos():
+    zt = tt.cos(xt)**2 + tt.sin(xt)**2
+    zs = theano_to_sympy(zt)
+    m = shape_and_dtype_map(zt)
+    assert sympy_to_theano(sympy.simplify(zs), m) == 1
